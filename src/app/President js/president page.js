d2a4aaa211f4544aa1a842bd6bf8084860c25992
kahunde-Elizabeth <kahundeliz7@gmail.com>
2025-03-05 11:04:59 +0300

    commit 4

---
+++ src/app/President js/president page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import President from "./dashboards/president";
+ export default function Page(){
+  return(
+    <div>
+    <President />
+    </div>
+        
+ );
+}
--- src/app/page2.js
+++
@@ -1,9 +0,0 @@
-import React from "react"; 
-import President from "./dashboards/president.js";
- export default function Page() {
-  return(
-    <div>
-      <President/>
-    </div>
-  )
-};
